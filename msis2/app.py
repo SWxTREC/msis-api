751252374b0d4f01128b0bf36bf35d0705bc61b1
Greg Lucas <greg.lucas@lasp.colorado.edu>
2020-08-03 17:08:10 -0600

    ENH: Adding new endpoints to the MSIS API
    - Surface endpoint to return data at a specific altitude level
    - Altitude endpoint to return data at a specific (lon, lat)
    
    These new endpoints will receive data encoded in the URL as
    query parameters rather than in the body of the request.

--- msis2/app.py
+++ msis2/app.py
@@ -61,6 +61,7 @@ Default is for all of them to be set to 1
 !                           15-25 - Not used in NRLMSIS 2.0
 """
 from datetime import datetime
+# import geojson
 import json
 import sys
 import os
@@ -96,6 +97,17 @@ def validate_event(event):
 
 def lambda_handler(event, context):
     """Handle the incoming API event and route properly."""
+
+    # Parse the incoming path and route the event properly
+    if 'surface' in event['path']:
+        return surface_handler(event, context)
+
+    if 'altitude' in event['path']:
+        return altitude_handler(event, context)
+
+
+    print("GML EVENT:", event)
+    print("GML CONTEXT:", context)
     # Data is passed in the body, so pull it out of there
     data = json.loads(event['body'])
     # Validate the event
@@ -119,3 +131,210 @@ def lambda_handler(event, context):
         'body': json.dumps(output.tolist()),
         "headers": {"Access-Control-Allow-Origin": "*"}
     }
+
+
+def altitude_handler(event, context):
+    pass
+
+
+def surface_handler(event, context):
+    """Handle the incoming API event and route properly."""
+    # Data is passed in the URL, so pull it out of there
+    params = event['queryStringParameters']
+    date = datetime.strptime(params['date'], "%Y-%m-%dT%H:%M")
+    altitude = float(params['altitude'])
+    f107 = float(params['f107'])
+    f107a = float(params['f107a'])
+    ap = float(params['ap'])
+    # Initialize the model, default is all ones
+    options = params.get('options', [1]*25)
+    if isinstance(options, str):
+        # Extract the list input out of the string storage
+        options = json.loads(options)
+
+    if len(options) != 25:
+        raise ValueError("options requires a length 25 array")
+    init_msis(options)
+
+    # Make the 5 degree x 5 degree grid (center points)
+    lats = [x + 2.5 for x in range(-90, 90, 5)]
+    lons = [x + 2.5 for x in range(-180, 180, 5)]
+
+    # Call the main loop
+    output = run_msis([date], lons, lats, [altitude], [f107], [f107a], [ap])
+
+    # geo_output = surface_geojson(data, output)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(output.tolist()),
+        "headers": {"Access-Control-Allow-Origin": "*"}
+    }
+
+
+def altitude_handler(event, context):
+    """Handle the incoming API event and route properly."""
+    # Data is passed in the URL, so pull it out of there
+    params = event['queryStringParameters']
+    date = datetime.strptime(params['date'], "%Y-%m-%dT%H:%M")
+    longitude = float(params['longitude'])
+    latitude = float(params['longitude'])
+    f107 = float(params['f107'])
+    f107a = float(params['f107a'])
+    ap = float(params['ap'])
+    # Initialize the model, default is all ones
+    options = params.get('options', [1]*25)
+    if isinstance(options, str):
+        # Extract the list input out of the string storage
+        options = json.loads(options)
+
+    if len(options) != 25:
+        raise ValueError("options requires a length 25 array")
+    init_msis(options)
+
+    # Make the list of altitudes to use
+    alts = [x for x in range(100, 1005, 5)]
+
+    # Call the main loop
+    output = run_msis([date], [longitude], [latitude], alts, [f107], [f107a],
+                      [ap])
+
+    # Parse down the output data to only what we need
+    # (altitude, species data)
+    output = output[0, 0, 0, :, :]
+    features = {}
+    for i in range(len(alts)):
+        alt = alts[i]
+
+        props = {'Mass': output[i, 0],
+                 'N2': output[i, 1],
+                 'O2': output[i, 2],
+                 'O': output[i, 3],
+                 'He': output[i, 4],
+                 'H': output[i, 5],
+                 'Ar': output[i, 6],
+                 'N': output[i, 7],
+                 'AnomO': output[i, 8],
+                 'NO': output[i, 9],
+                 'Temperature': output[i, 10]}
+        features[alt] = props
+
+    return {
+        'statusCode': 200,
+        'body': features,
+        "headers": {"Access-Control-Allow-Origin": "*"}
+    }
+
+
+def surface_geojson(data, output):
+    """Create a geojson format for the surface request."""
+    # Parse down the output data to only what we need
+    # (lons, lats, species data)
+    output = output[0, :, :, 0, :]
+    features = []
+    for i in range(data['lons']):
+        lon = data['lons'][i]
+        for j in range(data['lats']):
+            lat = data['lats'][j]
+            poly = geojson.Polygon([[lon-2.5, lat-2.5], [lon-2.5, lat+2.5],
+                                    [lon+2.5, lat+2.5], [lon+2.5, lat-2.5],
+                                    [lon-2.5, lat-2.5]])
+            props = {'Mass': output[i, j, 0],
+                     'N2': output[i, j, 1],
+                     'O2': output[i, j, 2],
+                     'O': output[i, j, 3],
+                     'He': output[i, j, 4],
+                     'H': output[i, j, 5],
+                     'Ar': output[i, j, 6],
+                     'N': output[i, j, 7],
+                     'AnomO': output[i, j, 8],
+                     'NO': output[i, j, 9],
+                     'Temperature': output[i, j, 10]}
+            feat = geojson.Feature(geometry=poly, properties=props)
+            features.append(feat)
+
+    return geojson.FeatureCollection(features)
+
+
+def altitude_json(data, output):
+    """Create a json format for the altitude request."""
+    # Parse down the output data to only what we need
+    # (lons, lats, species data)
+    output = output[0, 0, 0, :, :]
+    features = {}
+    for i in range(data['alts']):
+        alt = data['alts'][i]
+
+        props = {'Mass': output[i, 0],
+                 'N2': output[i, 1],
+                 'O2': output[i, 2],
+                 'O': output[i, 3],
+                 'He': output[i, 4],
+                 'H': output[i, 5],
+                 'Ar': output[i, 6],
+                 'N': output[i, 7],
+                 'AnomO': output[i, 8],
+                 'NO': output[i, 9],
+                 'Temperature': output[i, 10]}
+        features[alt] = props
+
+    return features
+
+
+def test():
+    """Create a geojson format for the surface request."""
+    # Parse down the output data to only what we need
+    # (lons, lats, species data)
+    lats = [x+2.5 for x in range(-90, 90, 5)]
+    lons = [x+2.5 for x in range(-180, 180, 5)]
+    output = np.random.random_sample((len(lons), len(lats), 11))
+    features = []
+    for i in range(len(lons)):
+        lon = lons[i]
+        for j in range(len(lats)):
+            lat = lats[j]
+            poly = geojson.Polygon([[lon-2.5, lat-2.5], [lon-2.5, lat+2.5],
+                                    [lon+2.5, lat+2.5], [lon+2.5, lat-2.5],
+                                    [lon-2.5, lat-2.5]])
+            props = {'Mass': output[i, j, 0],
+                     'N2': output[i, j, 1],
+                     'O2': output[i, j, 2],
+                     'O': output[i, j, 3],
+                     'He': output[i, j, 4],
+                     'H': output[i, j, 5],
+                     'Ar': output[i, j, 6],
+                     'N': output[i, j, 7],
+                     'AnomO': output[i, j, 8],
+                     'NO': output[i, j, 9],
+                     'Temperature': output[i, j, 10]}
+            props = {'data': output[i, j, :].tolist()}
+            feat = geojson.Feature(geometry=poly, properties=props)
+            features.append(feat)
+
+    return geojson.FeatureCollection(features)
+
+
+def test2():
+    """Create a geojson format for the surface request."""
+    # Parse down the output data to only what we need
+    # (lons, lats, species data)
+    lats = [x+2.5 for x in range(-90, 90, 5)]
+    lons = [x+2.5 for x in range(-180, 180, 5)]
+    output = np.random.random_sample((len(lons), len(lats), 11))
+    geojson.MultiPolygon()
+
+    coords = []
+    data = []
+    for i in range(len(lons)):
+        lon = lons[i]
+        for j in range(len(lats)):
+            lat = lats[j]
+            coords.append([[lon-2.5, lat-2.5], [lon-2.5, lat+2.5],
+                                    [lon+2.5, lat+2.5], [lon+2.5, lat-2.5],
+                                    [lon-2.5, lat-2.5]])
+            data.append(output[i, j, :].tolist())
+
+    poly = geojson.MultiPolygon(coordinates=coords)
+    feature = geojson.Feature(geometry=poly, properties={"data": data})
+
+    return geojson.FeatureCollection(feature)
